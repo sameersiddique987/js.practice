3586dccb8d8a19ccf9b64eddfe7a2d1938694918
sameersiddique987 <sameersiddique301@gmail.com>
2024-02-22 15:01:31 +0500

    new file

---
+++ app.js
@@ -0,0 +1,15 @@
+ alert("hello world");
+
+
+ var firstName = ("sameer")
+ var lastName = ("siddique")
+var age = (19)
+var isAvailable = (true)
+
+
+ console.log( "my name is" +" "+  firstName+" "+lastName +" "+ "my age" +" "+ age +" "+ isAvailable)
+
+
+
+
+
