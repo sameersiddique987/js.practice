e0cf7d5f6eb11384cd8d5f635f4b0d6598e19668
sameersiddique987 <sameersiddique301@gmail.com>
2024-02-24 15:52:22 +0500

    created file

--- app.js
+++ app.js
@@ -1,15 +1,117 @@
- alert("hello world");
+//  alert("hello world");
 
 
- var firstName = ("sameer")
- var lastName = ("siddique")
-var age = (19)
-var isAvailable = (true)
+//  var firstName = ("sameer")
+//  var lastName = ("siddique")
+// var age = (19)
+// var isAvailable = (true)
 
 
- console.log( "my name is" +" "+  firstName+" "+lastName +" "+ "my age" +" "+ age +" "+ isAvailable)
-
-
+//  console.log( "my name is" +" "+  firstName+" "+lastName +" "+ "my age" +" "+ age +" "+ isAvailable)
 
 
 
+// var num1 = 10;
+// var num2 = 20;
+// var num3 = "10";
+// console.log(typeof(num1));
+// console.log(num1 + num2 + num3)
+
+// var userName = prompt('Inter your name');
+// console.log('userName');
+
+// var num1 = Number(prompt('Inter your First name'))
+// var num2 = Number (prompt('Iter your Second name'))
+
+// var num1 =+ prompt('Inter your First name')
+// var num2 =+ prompt('Inter your Second name')
+// console.log(num1 + num2)
+
+
+
+// Arithmatic Operator
+
+// +
+// -
+// *
+// /
+// % == modulus
+
+// var num1 = 10;
+// var num2 = 5;
+
+// console.log(num1 + num2)
+// console.log(num1 - num2)
+// console.log(num1 * num2)
+// console.log(num1 / num2)
+// console.log(num1 % num2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Assignment Operator
+// =
+// +=
+// -=
+// /=
+// *=
+
+var num1 = 12;
+// num1 = num1 + 10;
+// num1 += 10;
+// num1 -= 10;
+// num1 /= 10;
+// num1 *= 10;
+// console.log(num1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Comparision Operator
+
+// >
+// <
+// == || !=
+// === || !==
+// != data type check nahi kraiga!
+// !== data type check kraiga!
+// >=
+// <=
+
+
+ 
+// console.log(10 == '10')
+// console.log(10 === 10)
+
+var userName =("sameer")
+document.write( '<h1>' + userName + '</h1>')
