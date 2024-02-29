5e8f20172f5a81f9d084e40e26a4c9f4c32d6005
sameersiddique987 <sameersiddique301@gmail.com>
2024-02-29 20:04:40 +0500

    created file

--- app.js
+++ app.js
@@ -76,7 +76,7 @@
 // /=
 // *=
 
-var num1 = 12;
+// var num1 = 12;
 // num1 = num1 + 10;
 // num1 += 10;
 // num1 -= 10;
@@ -113,5 +113,158 @@ var num1 = 12;
 // console.log(10 == '10')
 // console.log(10 === 10)
 
+
+
+//logical Operator
+
+//&& == and
+//|| == OR
+
+
+// var comparision = 10 <= 10 || 10 < 6 || 10 > 15;
+// var comparision = 10 <= 10 && 10 < 6 && 10 > 15;
+// console.log(comparision);
+
+var num = 10;
+console.log(num+1)
+console.log(++num)
+console.log(num++)
+
+console.log(num++)
+console.log(++num)
+
+
+console.log(++num + num++ + num++ + num++ + ++num)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 var userName =("sameer")
 document.write( '<h1>' + userName + '</h1>')
+
+
+
+
+
+
+
+
+
+
+
+// var bankBalance = +prompt('enter your bank balance');
+
+// if(bankBalance > 1500000){
+//     console.log('rishta mubarak ho!')
+// }else{
+//     console.log('ammi na zehr kahlia! || istekhara ma mana hogaya ha.')
+// }
+
+
+
+
+
+
+
+
+
+
+// class 3
+
+// var percentage = +prompt("inter your percentage")
+
+// if(percentage > 100){
+//     console.log("malik percentage tw sahe do!")
+// }
+// else if(percentage < 50){
+//     console.log("malik rikshaw chalao")
+// }
+// else if(percentage >= 50 && percentage <= 60){
+//     console.log("C grade aya h")
+// }
+// else if(percentage >= 60 && percentage <= 70){
+//     console.log("B grade aya h")
+// }
+// else if(percentage >= 70 && percentage <= 80){
+//     console.log("A grade aya h")
+// }
+// else if(percentage >= 80 && percentage <= 100){
+//     console.log("A1 grade aya h")
+// }
+// else{
+//     console.log(percentage)
+// }
+
+
+
+
+
+
+
+
+
+
+
+var num1 = 2;
+if(num1>8){
+    console.log("8 num se bara h")  
+}
+else if(num1 > 6){
+    console.log("6 num bara h")
+}
+else{
+    console.log("2 num se chuta h")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
