10879869facdda31746b00c2ae40b703f691d62d
sameersiddique987 <sameersiddique301@gmail.com>
2024-03-14 11:26:06 +0500

    updated file

---
+++ Array/app.js
@@ -0,0 +1,260 @@
+// let city1 = "karachi"
+// let city2 = "lahore"
+// let city3 = "islamabad"
+
+// // let cities = ["karachi" , "lahore" , "islamabad"]
+// let cities = ["karachi" , "lahore" , "islamabad" , 34 , true , false , ["fb area " , "lyari" , "golimar"]]
+// console.log(cities[8][2])
+// console.log(typeof(cities))
+// console.log(cities[3])
+
+
+// 
+
+
+// const user = ['abdullah' , 'usman' , 'haider'];
+// // console.log(`${user[0]} ${user[2]}`);
+
+// user[0] = 'ammar';
+// user[4] = 'ali'
+// console.log(user[3])
+
+
+
+
+
+
+
+
+
+
+
+
+// const user = []
+// user[0]=prompt("enter name1" )
+// user[1]=prompt("enter name2" )
+// user[2]=prompt("enter name3" )
+// console.log(user)
+
+
+
+
+
+// // const fruit ="apple"
+// // const fruits = ["banana" , "mango" , "orange" , fruit]
+
+// console.log(fruits)
+
+
+
+
+
+
+
+
+
+
+
+//push
+//pop
+//unshift
+//shift
+//splice
+//slice
+//split
+//join
+//concat
+//sort
+//reverse
+//indexof
+// lastIndexof
+
+// const fruits  = ["mango" , "apple" , "banana"]
+// // fruits.push(watermelon)
+// fruits.pop() 
+// fruits.unshift('watermelon')
+// fruits.shift()
+
+
+
+
+
+
+
+// fruits.splice('shurw kaha sa krna ha' , 'kitna delete krna hain' , 'uski jaga kiya rkhna haa')
+
+
+// const fruits  = ["mango" , "watermelon" , "graps" , 'orange']
+// fruits.splice(1 ,  2,'strowberry' , 'anar', 'mango' , 'apple' , 'banana' , 'gawa' )
+// // fruits.splice(2 , 3)
+// // fruits.splice(6 , 2 , 'banana');
+// fruits[3].pop()
+// console.log(fruits)
+
+
+
+
+// const fruits  = ["mango" , "watermelon" , "graps" , 'orange']
+//  const newArr = fruits.slice(2)
+// console.log(newArr)
+// console.log(fruits)
+
+
+
+
+
+// const names = "Muhammad sameer siddique"
+// const newArr = names.split(' ')
+// console.log(newArr)
+
+
+
+
+
+
+// const names = ["Muhammad" , "sameer" , "siddique"]
+// const newArr = names.join("  ")
+// console.log(newArr)
+
+
+
+
+
+// const arry1 = ["sunday" , "monday" , "tuesday" , "wednesday"]
+// const arry2 = ["friday" , "saturday"]
+
+// const arry3 = arry1.concat("thusday", arry2)
+// console.log(arry3)
+
+
+
+
+
+
+// const arry1 = ["B" , "A" , "C" , "D" ]
+// const arry2 = arry1.sort().reverse()
+// console.log(arry2)
+
+
+
+
+
+
+// const arry1 = ["sameer" , "ammar" , "ali" , "sameer" , "ammar" , "abudllah" ]
+// const arry2 = arry1.indexOf("sameer",3)
+// console.log(arry2)
+
+
+
+
+
+
+// const arry1 = ["sameer" , "ammar" , "ali" , "sameer" , "ammar" , "abudllah" ]
+// const arry2 = arry1.lastIndexOf("ammar" ,3)
+// console.log(arry2)
+
+
+
+
+
+
+// const fruits  = ["mango" , "watermelon" , "graps" , "orange", "gawa" , "banana"]
+// fruits.splice(1 , 1 ,"sunday")
+// fruits.splice(3 , 1 , "fruit 1" ,"fruit 2" )
+
+ 
+// console.log(fruits)
+
+
+
+
+
+
+const shop  = ["mango" , "watermelon" , "graps" , "orange", "gawa" , "banana"]
+console.log(shop.includes("mango"))
+
+
+
+
+
+
+
+
+
+
+
+// const shop  = ["mango" , "watermelon" , "graps" , "orange", "gawa" , "banana"]
+// const input = document.querySelector("#input")
+// const h2 = document.querySelector("h2")
+// function getshop (){
+//     console.log(input.value)
+//     if(shop.includes(input.value)){
+//         console.log("fruits avilaible h")
+//         h2.innerHTML=`${input.value} is avilaible`
+//     }
+//     else{
+//         console.log("fruite avilaible nahe h..")
+//         h2.innerHTML=`${input.value} is not avilaible`
+//     }
+//     input.value = ""
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const shop  = ["mango" , "watermelon" , "graps" , "orange", "gawa" , "banana"]
+// const input = document.querySelector("input")
+// const h2 = document.querySelector("h2")
+// function getshop(){
+//     console.log(input.value)
+//     if(shop.includes(input.value)){
+//         console.log("avilable h")
+//         h2.innerHTML=`${input.value} is avilable`
+//     }
+//     else{
+//         console.log("avilable nahe h ")
+//         h2.innerHTML=`${input.value}is not avilable`
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
