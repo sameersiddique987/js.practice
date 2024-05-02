4240f06072c6a1a980d16846a0f42c4d36c2e405
sameersiddique987 <sameersiddique301@gmail.com>
2024-05-02 20:52:24 +0500

    created file

---
+++ loops/app.js
@@ -0,0 +1,164 @@
+// console.log("hello loops")
+
+
+// // for (let i =11; i> 10; i++) {
+// //     console.log(i)
+    
+// }
+
+// hang computer ^
+
+// for (let i =1; i<= 10; i++) {
+//     console.log(i)
+    
+// }
+
+
+
+// const userInput =+ prompt("inter table number")
+// const times =+ prompt("how many time do you want?")
+
+// for (let i = 1; i <= times; i++) {
+//     console.log(`${userInput}*${i}=${userInput*i}`)
+    
+// }
+
+
+
+
+
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log("outer loop==>",i)
+//     for (let j = 0; j <= 10; j++) {
+//         console.log("inner loop",i)
+        
+//     }
+    
+// }
+
+
+
+
+
+
+
+// let fruits = ['apple' , 'banana' , 'orange' , 'watermelon' , 'grapes'];
+// console.log(fruits)
+// let ul = document.querySelector("ul")
+// for (let i = 0; i < fruits.length; i++) {
+//     // confirm.log(fruits[i])
+//     ul.innerHTML=`<li>${fruits[i]}</li>`
+// }
+
+
+
+
+
+
+// const fruits = ["apple", "banana", "orange", "grape", "kiwi"];
+
+// let ul = document.querySelector("ul")
+// function getFruits (){
+// ul.innerHTML=""
+// for (let i = 0; i < fruits.length; i++) {
+// ul.innerHTML +=`<li>${fruits[i]}</li>`
+    
+// }
+
+
+// }
+
+
+
+
+
+
+
+
+
+
+// const fruits = [
+//     "Apple",
+//     "Banana",
+//     "Orange",
+//     "Grape",
+//     "Strawberry",
+//     "Pineapple",
+//     "Watermelon",
+//     "Mango",
+//     "Kiwi",
+//     "Pear",
+//     "Peach",
+//     "Cherry",
+//     "Plum",
+//     "Blueberry",
+//     "Raspberry",
+//     "Blackberry",
+//     "Papaya",
+//     "Coconut",
+//     "Lemon",
+//     "Lime"
+//   ];
+  
+//   console.log(fruits);
+// const ol = document.querySelector("ol")
+// function getFruits() {
+//     ol.innerHTML=""
+
+//     for (let i = 0; i < fruits.length; i++) {
+
+//         ol.innerHTML += `<li>${fruits[i]}</li>`
+//     }
+        
+//     }
+
+
+
+
+
+// let j = 11;
+// while(j < 10){
+//     console.log("while loop===>",j);
+//     j++;
+// }
+
+
+
+
+
+// let i = 11;
+// do{
+//     console.log("do while loop==>",i);
+// }while(i < 10)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
