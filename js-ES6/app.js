65cd52790ec80ff6e6ad8986487e3d163371e072
sameersiddique987 <sameersiddique301@gmnail.com>
2024-06-04 12:41:55 +0500

    updated file

---
+++ js-ES6/app.js
@@ -0,0 +1,107 @@
+// console.log("hello world");
+// Variable Scoping
+// Closure
+// Generators
+// Modules
+// Exponentiation Operator
+// Optional chaining
+// Object Methods (keys,values,freez,entries)
+// Destructuring //p
+// Template literals //p
+// Destructuring //p
+// Default parameters //p
+// Rest parameter //p
+// Spread Operator //p
+// Arrow functions  //p
+// Enhanced object literals //p
+// Iterators & For..of  //p
+// Map  //p
+// Array methods  //p
+// Higher-order function  //p
+// CallBack  //p
+// Promises  //p
+// Ternary Operator //p
+
+//  user = {
+//     username:" sameer ",
+//     email: "sameersiddique301@gmail.com" ,
+//     age: 19
+// }
+// const {username,email,age} = user
+// console.log(username , email , age); 
+
+//const fruit = ["apple", "mango", "banana","watermelon","graps" ]
+//const [,,,four]=fruit
+// console.log(four);
+
+//spread operator
+
+
+// const fruit = ["apple", "mango", "banana","watermelon","graps" ]
+// const arr2 = ["nashpati",...fruit]
+ //console.log(arr2);
+// const newarr = arr2.concat(fruit)
+// console.log(newarr);
+
+
+//rest operator
+
+// function hello(...username) {
+//     console.log(username);
+// }
+// hello("sameer","usman","shoeb","zohaib")
+
+
+//ternary operator
+
+
+// const num = 30;
+// if (num > 50) {
+//     console.log("passed");
+// }
+// else{
+//     console.log("failed");
+// }
+
+// num > 50 ? console.log("passed") : console.log("failed");
+
+
+//  if (num > 50) {
+//     console.log("passed");
+// }
+// else if (num > 40){
+// console.log("dhakka passed");
+// }
+// else{
+//     console.log("failed");
+// }
+
+
+// num > 50 ? console.log("passed"):num > 40 ? console.log("dhakka passed"):console.log("failed");
+
+
+//default parameter
+
+
+// function user(username="abc") {
+//     console.log(username);
+// }
+// user()
+
+
+// Enhanced object literals
+
+
+// let names = "sameer"
+// let age = 19
+
+// let user = {
+//     names,
+//     age
+// }
+// console.log(user);
+
+
+
+
+// rgffsejfoisdfjds0otfjesaiodjaPODWJREAOPFKRPOIJGTEWORJTPEOSMMFJIOSDJFOPASADGJ
